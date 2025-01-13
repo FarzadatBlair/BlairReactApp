3b4edb7bb275226160f0b180b65dab69811f7dbe
Farzad R <farzadrahman20@gmail.com>
2025-01-13 14:03:09 -0500

    Co-authored-by: Kevin Zhang <kevinzhang03@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,23 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       colors: {
-        background: "#FFFBF2",
-        primary: "#514134",
+        primary: {
+          DEFAULT: '#514134',
+          200: '#D2CDC6',
+          300: '#978B80',
+          400: '#74665A',
+          500: '#514134',
+          900: '#30241D',
+        },
+        background: '#FFFBF2',
       },
       fontFamily: {
         sans: ['Inter', 'sans-serif'],
