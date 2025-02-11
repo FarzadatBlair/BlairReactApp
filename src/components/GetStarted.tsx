7ccc66ad359ed9c6f23167b8eb82f765c9e5e430
kevinz-blair <kevinz.blair@gmail.com>
2025-02-11 04:19:44 -0500

    refactor get-started page, add button disable states

--- src/components/GetStarted.tsx
+++ src/components/GetStarted.tsx
@@ -1,9 +1,8 @@
 'use client';
 
-import { useState } from 'react';
+import { useState, useCallback, useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { supabase } from '@utils/supabase/supabase';
-import { logout } from '@/utils/actions';
 
 import GenericPage from '@components/layout/GenericPage';
 import Button from '@components/common/Button';
@@ -16,7 +15,7 @@ import {
   ProvinceCode,
 } from '@/utils/constants';
 
-interface formDataType {
+interface FormDataType {
   firstName: string;
   lastName: string;
   dob: string;
@@ -30,7 +29,7 @@ interface formDataType {
 const GetStarted = () => {
   const router = useRouter();
   const [step, setStep] = useState(1);
-  const [formData, setFormData] = useState<formDataType>({
+  const [formData, setFormData] = useState<FormDataType>({
     firstName: '',
     lastName: '',
     dob: '',
@@ -40,269 +39,295 @@ const GetStarted = () => {
     healthcareProvince: null,
     healthcareNumber: '',
   });
-  const [isHealthcareNumberValid, setIsHealthcareNumberValid] =
-    useState<boolean>(true);
+  const [isHealthcareNumberValid, setIsHealthcareNumberValid] = useState(true);
   const [errorMessage, setErrorMessage] = useState('');
+  const [healthCareError, setHealthCareError] = useState('');
+  const [isStepOneValid, setIsStepOneValid] = useState(false);
 
-  const handleSignOut = async () => {
-    await logout();
-    router.replace('/');
-  };
+  useEffect(() => {
+    const requiredFields: (keyof FormDataType)[] = [
+      'firstName',
+      'lastName',
+      'dob',
+      'gender',
+      'address',
+      'province',
+    ];
+    const isValid = requiredFields.every((field) => formData[field]);
+    setIsStepOneValid(isValid);
+  }, [formData]);
 
-  const handleSubmit = async () => {
-    if (step === 1) {
-      const requiredFields: (keyof formDataType)[] = [
-        'firstName',
-        'lastName',
-        'dob',
-        'gender',
-        'address',
-        'province',
-      ];
-      const isFormValid = requiredFields.every((field) => formData[field]);
-
-      if (!isFormValid) {
-        setErrorMessage('Please fill out all required fields.');
-        return;
+  const validateHealthcareNumber = useCallback(
+    (number: string) => {
+      if (formData.healthcareProvince === null) {
+        return true;
       }
 
-      setErrorMessage('');
-      setStep(2);
-    } else {
-      if (formData.healthcareProvince && !formData.healthcareNumber) {
-        setErrorMessage(
-          'Healthcare number is required if a healthcare province is selected.',
+      const regex = HEALTH_CARD_FORMATS[formData.healthcareProvince]?.regex;
+      const healthCardName =
+        HEALTH_CARD_FORMATS[formData.healthcareProvince]?.name;
+
+      if (!regex) {
+        return true;
+      }
+
+      const isValid = regex.test(number);
+      if (!isValid) {
+        setHealthCareError(
+          `Invalid ${healthCardName} format. Example: ${HEALTH_CARD_FORMATS[formData.healthcareProvince]?.example}`,
         );
-        return;
       }
-
-      setErrorMessage('');
-
-      try {
-        // Extract necessary fields
-        const {
-          firstName,
-          lastName,
-          dob,
-          gender,
-          address,
-          province,
-          healthcareProvince,
-          healthcareNumber,
-        } = formData;
-
-        // Insert into `users` table
-        const { data: userInsertData, error: userInsertError } = await supabase
-          .from('users')
-          .insert([
-            {
-              user_id: (await supabase.auth.getUser()).data.user?.id, // Get the current user's ID
-              first_name: firstName,
-              last_name: lastName,
-              dob,
-              sex: gender,
-              address,
-              province,
-            },
-          ]);
-
-        if (userInsertError) {
-          console.error(userInsertError);
-          setErrorMessage('Failed to save user information. Please try again.');
-          return;
-        }
-
-        console.log('User data inserted:', userInsertData);
-
-        // If healthcare info is provided, insert into `user_healthcare` table
-        if (healthcareProvince && healthcareNumber) {
-          const { data: healthcareInsertData, error: healthcareInsertError } =
-            await supabase.from('user_healthcare').insert([
-              {
-                user_id: (await supabase.auth.getUser()).data.user?.id, // Same user ID
-                healthcare_province: healthcareProvince,
-                healthcare_number: healthcareNumber,
-              },
-            ]);
-
-          if (healthcareInsertError) {
-            console.error(healthcareInsertError);
-            setErrorMessage(
-              'Failed to save healthcare information. Please try again.',
-            );
-            return;
-          }
-
-          console.log('Healthcare data inserted:', healthcareInsertData);
-        }
-
-        // Redirect or notify success
-        console.log('Data submitted successfully!');
-        router.push('/');
-      } catch (err) {
-        console.error('Error submitting data:', err);
-        setErrorMessage('An error occurred. Please try again.');
-      }
-    }
-  };
+      return isValid;
+    },
+    [formData.healthcareProvince, setHealthCareError],
+  );
 
   const handleChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
   ) => {
     const { name, value } = e.target;
-    setFormData((prev) => ({
-      ...prev,
-      [name]: value,
-    }));
+    setFormData((prev) => ({ ...prev, [name]: value }));
+
+    if (name === 'healthcareProvince' || name === 'healthcareNumber') {
+      setHealthCareError('');
+      setIsHealthcareNumberValid(true);
+    }
   };
 
   const handleBlur = (
     e: React.FocusEvent<HTMLInputElement | HTMLSelectElement>,
   ) => {
     const { name, value } = e.target;
-
     if (name === 'healthcareNumber') {
-      const isValid = validateHealthcareNumber(value);
-      // setFormData((prev) => ({
-      //   ...prev,
-      //   isHealthcareNumberValid: isValid,
-      // }));
-      setIsHealthcareNumberValid(isValid);
+      setIsHealthcareNumberValid(validateHealthcareNumber(value));
     }
   };
 
-  const validateHealthcareNumber = (number: string) => {
-    // Ensure healthcareProvince is not null before accessing HEALTH_CARD_FORMATS
-    if (formData.healthcareProvince !== null) {
-      const regex = HEALTH_CARD_FORMATS[formData.healthcareProvince]?.regex;
-      const healthCardName =
-        HEALTH_CARD_FORMATS[formData.healthcareProvince]?.name;
-      if (regex) {
-        const isValid = regex.test(number);
-        if (!isValid) {
-          setErrorMessage(
-            `Invalid ${healthCardName} format. Example: ${HEALTH_CARD_FORMATS[formData.healthcareProvince]?.example}`,
-          );
-        }
-        return isValid;
+  const handleStepOneSubmit = () => {
+    if (!isStepOneValid) {
+      setErrorMessage('Please fill out all required fields.');
+      return;
+    }
+
+    setErrorMessage('');
+    setStep(2);
+  };
+
+  const handleStepTwoSubmit = async () => {
+    if (formData.healthcareProvince && !formData.healthcareNumber) {
+      setErrorMessage(
+        'Healthcare number is required if a healthcare province is selected.',
+      );
+      return;
+    }
+
+    if (
+      formData.healthcareProvince &&
+      formData.healthcareNumber &&
+      !isHealthcareNumberValid
+    ) {
+      setErrorMessage('Please correct the healthcare number format.');
+      return;
+    }
+
+    setErrorMessage('');
+
+    try {
+      const {
+        firstName,
+        lastName,
+        dob,
+        gender,
+        address,
+        province,
+        healthcareProvince,
+        healthcareNumber,
+      } = formData;
+
+      const userId = (await supabase.auth.getUser()).data.user?.id;
+
+      const { error: userInsertError } = await supabase.from('users').insert([
+        {
+          user_id: userId,
+          first_name: firstName,
+          last_name: lastName,
+          dob,
+          sex: gender,
+          address,
+          province,
+        },
+      ]);
+
+      if (userInsertError) {
+        console.error(userInsertError);
+        setErrorMessage('Failed to save user information. Please try again.');
+        return;
       }
+
+      if (healthcareProvince && healthcareNumber) {
+        const { error: healthcareInsertError } = await supabase
+          .from('user_healthcare')
+          .insert([
+            {
+              user_id: userId,
+              healthcare_province: healthcareProvince,
+              healthcare_number: healthcareNumber,
+            },
+          ]);
+
+        if (healthcareInsertError) {
+          console.error(healthcareInsertError);
+          setErrorMessage(
+            'Failed to save healthcare information. Please try again.',
+          );
+          return;
+        }
+      }
+
+      router.push('/');
+    } catch (err) {
+      console.error('Error submitting data:', err);
+      setErrorMessage('An error occurred. Please try again.');
     }
-    // If healthcareProvince is null, return true as no validation is needed
-    return true;
   };
 
-  const handleBack = () => {
-    setStep(1);
+  const handleSubmit = () => {
+    if (step === 1) {
+      handleStepOneSubmit();
+    } else {
+      handleStepTwoSubmit();
+    }
   };
+
+  const renderStepOne = () => (
+    <>
+      <div>
+        <h1>
+          Tell us a bit
+          <br />
+          about yourself
+        </h1>
+        <p className="mt-4">Please fill out all fields.</p>
+      </div>
+      <div className="flex w-full flex-grow flex-col space-y-4">
+        <Input
+          type="text"
+          name="firstName"
+          value={formData.firstName}
+          onChange={handleChange}
+          placeholder="First Name"
+        />
+        <Input
+          type="text"
+          name="lastName"
+          value={formData.lastName}
+          onChange={handleChange}
+          placeholder="Last Name"
+        />
+        <Input
+          type="date"
+          name="dob"
+          value={formData.dob}
+          onChange={handleChange}
+          placeholder="Date of Birth"
+        />
+        <Input
+          type="select"
+          name="gender"
+          onChange={handleChange}
+          value={formData.gender}
+          placeholder="Select Gender"
+          options={[
+            { value: 'M', label: 'Male' },
+            { value: 'F', label: 'Female' },
+          ]}
+        />
+        <Input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+          placeholder="Address"
+        />
+        <Input
+          type="select"
+          name="province"
+          value={formData.province ?? ''}
+          onChange={handleChange}
+          placeholder="Province"
+          options={PROVINCES}
+          className="w-full"
+        />
+      </div>
+      <div className="flex flex-col space-y-4">
+        <div>
+          <Button onClick={handleSubmit} disabled={!isStepOneValid}>
+            Continue
+          </Button>
+          {errorMessage && (
+            <p className="mt-2 text-error-500">{errorMessage}</p>
+          )}
+        </div>
+      </div>
+    </>
+  );
+
+  const renderStepTwo = () => (
+    <>
+      <div>
+        <h1>What&apos;s your healthcare coverage plan?</h1>
+        <p className="mt-4">Enter the province of your healthcare plan</p>
+      </div>
+      <div className="flex w-full flex-grow flex-col space-y-4">
+        <Input
+          type="select"
+          name="healthcareProvince"
+          value={formData.healthcareProvince ?? ''}
+          onChange={handleChange}
+          placeholder="Select Province"
+          options={PROVINCES}
+          className="w-full"
+        />
+        <Input
+          type="text"
+          name="healthcareNumber"
+          value={formData.healthcareNumber}
+          onChange={handleChange}
+          onBlur={handleBlur}
+          placeholder={
+            HEALTH_CARD_FORMATS[
+              formData.healthcareProvince as keyof typeof HEALTH_CARD_FORMATS
+            ]?.name || 'Enter healthcare number'
+          }
+          disabled={!formData.healthcareProvince}
+          error={!isHealthcareNumberValid}
+          errorMessage={healthCareError}
+        />
+      </div>
+      <div className="flex flex-col space-y-4">
+        <div>
+          <Button
+            onClick={handleSubmit}
+            disabled={
+              !isHealthcareNumberValid &&
+              formData.healthcareProvince !== null &&
+              formData.healthcareNumber !== ''
+            }
+          >
+            Submit
+          </Button>
+          {errorMessage && (
+            <p className="mt-2 text-error-500">{errorMessage}</p>
+          )}
+        </div>
+        <ButtonLink onClick={() => setStep(1)}>Back</ButtonLink>
+      </div>
+    </>
+  );
 
   return (
-    <GenericPage className="flex flex-col">
-      {step === 1 ? (
-        <>
-          <h1 className="mb-6">Tell us about yourself</h1>
-          <div className="flex w-full flex-col space-y-4">
-            <Input
-              type="text"
-              name="firstName"
-              value={formData.firstName}
-              onChange={handleChange}
-              placeholder="First Name"
-            />
-            <Input
-              type="text"
-              name="lastName"
-              value={formData.lastName}
-              onChange={handleChange}
-              placeholder="Last Name"
-            />
-            <Input
-              type="date"
-              name="dob"
-              value={formData.dob}
-              onChange={handleChange}
-              placeholder="Date of Birth"
-            />
-            <Input
-              type="select"
-              name="gender"
-              onChange={handleChange}
-              value={formData.gender}
-              placeholder="Select Gender"
-              options={[
-                { value: 'M', label: 'Male' },
-                { value: 'F', label: 'Female' },
-              ]}
-            />
-            <Input
-              type="text"
-              name="address"
-              value={formData.address}
-              onChange={handleChange}
-              placeholder="Address"
-            />
-            <Input
-              type="select"
-              name="province"
-              value={formData.province ?? ''}
-              onChange={handleChange}
-              placeholder="Province"
-              options={PROVINCES}
-              className="w-full"
-            />
-            <Button onClick={handleSubmit} className="mt-6">
-              Continue
-            </Button>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-            <div className="mt-4 text-center">
-              <ButtonLink onClick={handleSignOut} variant="secondary">
-                Sign Out
-              </ButtonLink>
-            </div>
-          </div>
-        </>
-      ) : (
-        <>
-          <h1 className="mb-2">What&apos;s your healthcare coverage plan?</h1>
-          <p className="">Enter the province of your healthcare plan</p>
-          <div className="mt-4 flex w-full flex-col space-y-4">
-            <Input
-              type="select"
-              name="healthcareProvince"
-              value={formData.healthcareProvince ?? ''}
-              onChange={handleChange}
-              placeholder="Select Province"
-              options={PROVINCES}
-              className="w-full"
-            />
-            <Input
-              type="text"
-              name="healthcareNumber"
-              value={formData.healthcareNumber}
-              onChange={handleChange}
-              onBlur={handleBlur}
-              placeholder={
-                HEALTH_CARD_FORMATS[
-                  formData.healthcareProvince as keyof typeof HEALTH_CARD_FORMATS
-                ]?.name || 'Enter healthcare number'
-              }
-              disabled={!formData.healthcareProvince}
-              error={!isHealthcareNumberValid}
-              errorMessage={errorMessage}
-            />
-            <Button onClick={handleSubmit} className="mt-6">
-              Submit
-            </Button>
-            {errorMessage && <p className="text-red-500">{errorMessage}</p>}
-            <div className="mt-4 text-center">
-              <ButtonLink onClick={handleBack}>Back</ButtonLink>
-              {' | '}
-              <ButtonLink onClick={handleSignOut} variant="secondary">
-                Sign Out
-              </ButtonLink>
-            </div>
-          </div>
-        </>
-      )}
+    <GenericPage className="flex flex-col space-y-6">
+      {step === 1 ? renderStepOne() : renderStepTwo()}
     </GenericPage>
   );
 };
